318d590a362a246fda5da729da651b24b55ff9b6
Maxime <maximebatista18@gmail.com>
2021-01-24 22:12:06 +0100

    Fixed a big parts of the bugs, added SharedCollection.set dans SharedCollection.buffer, added NetworkEntity.connectionDate, created network command

--- RelayController/src/fr/override/linkit/extension/controller/cli/commands/ShutdownCommand.scala
+++ RelayController/src/fr/override/linkit/extension/controller/cli/commands/ShutdownCommand.scala
@@ -5,5 +5,7 @@ import fr.`override`.linkit.api.Relay
 import fr.`override`.linkit.api.system.CloseReason
 
 class ShutdownCommand(relay: Relay) extends CommandExecutor {
-    override def execute(implicit args: Array[String]): Unit = relay.close(CloseReason.INTERNAL)
+    override def execute(implicit args: Array[String]): Unit = relay.runLater {
+        relay.close(CloseReason.INTERNAL)
+    }
 }
