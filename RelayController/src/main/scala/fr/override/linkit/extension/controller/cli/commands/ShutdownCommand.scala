031990a34008de1828ef3948edc7764ce86c59b8
Maxime <maximebatista18@gmail.com>
2021-03-21 14:53:07 +0100

    Renamed RelayPoint to Client and RelayServer to Server, Working on Server/Client implementations, complexioned Application configurations, added ExternalConnection in API, ServerConnection in Server and ClientConnection in Client

--- RelayController/src/main/scala/fr/override/linkit/extension/controller/cli/commands/ShutdownCommand.scala
+++ RelayController/src/main/scala/fr/override/linkit/extension/controller/cli/commands/ShutdownCommand.scala
@@ -1,12 +1,10 @@
 package fr.`override`.linkit.extension.controller.cli.commands
 
-import fr.`override`.linkit.api.Relay
-import fr.`override`.linkit.api.system.CloseReason
 import fr.`override`.linkit.extension.controller.cli.CommandExecutor
 
 class ShutdownCommand(relay: Relay) extends CommandExecutor {
     override def execute(implicit args: Array[String]): Unit = relay.runLater {
-        relay.close(CloseReason.INTERNAL)
+        relay.close(Reason.INTERNAL)
         System.exit(0)
     }
 }
