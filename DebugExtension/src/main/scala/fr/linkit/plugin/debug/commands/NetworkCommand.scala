66ca19121e8a00ba777c39f14651f8afac3e10e1
Maxime <maximebatista18@gmail.com>
2021-03-27 14:52:33 +0100

    Fixing network initialization

--- DebugExtension/src/main/scala/fr/linkit/plugin/debug/commands/NetworkCommand.scala
+++ DebugExtension/src/main/scala/fr/linkit/plugin/debug/commands/NetworkCommand.scala
@@ -24,31 +24,33 @@ class NetworkCommand(networks: => Iterable[Network]) extends CommandExecutor {
     }
 
     private def genDescription(network: Network): Unit = {
-        val entities = network.listEntities
-        val entitiesNames = entities.map(_.identifier).mkString(", ")
-        val count = entities.size
-        val upDate = network.startUpDate.toLocalDateTime
-        val self = network.connectionEntity
-        //val selfRemoteFragments = self.listRemoteFragmentControllers.map(_.nameIdentifier).mkString(", ")
-        val duration = getDurationAsString(upDate)
+        network.connection.runLater {
+            val entities      = network.listEntities
+            val entitiesNames = entities.map(_.identifier).mkString(", ")
+            val count         = entities.size
+            val upDate        = network.startUpDate.toLocalDateTime
+            val self          = network.connectionEntity
+            //val selfRemoteFragments = self.listRemoteFragmentControllers.map(_.nameIdentifier).mkString(", ")
+            val duration      = getDurationAsString(upDate)
 
-        println(s"${network.serverIdentifier}:")
-        println(s"There are $count relays connected on the network.")
-        println(s"Started at $upDate (Since: $duration)")
-        //println(s"Self entity : $self, enabled Remote Fragments : $selfRemoteFragments")
-        println(s"\tStatus : ${self.getConnectionState}")
-        println(s"All currently connected entities : $entitiesNames")
-        println("--------------------------------------------------")
-        println("For all entities : ")
-        entities.foreach(genDescription)
+            println(s"${network.serverIdentifier}:")
+            println(s"There are $count relays connected on the network.")
+            println(s"Started at $upDate (Since: $duration)")
+            //println(s"Self entity : $self, enabled Remote Fragments : $selfRemoteFragments")
+            println(s"\tStatus : ${self.getConnectionState}")
+            println(s"All currently connected entities : $entitiesNames")
+            println("--------------------------------------------------")
+            println("For all entities : ")
+            entities.foreach(genDescription)
+        }
     }
 
     private def genDescription(entity: NetworkEntity): Unit = {
-        val name = entity.identifier
+        val name               = entity.identifier
         //val remoteFragments = entity.listRemoteFragmentControllers.map(_.nameIdentifier).mkString(", ")
         //val apiVersion = entity.apiVersion
         //val implVersion = entity.relayVersion
-        val connectionDate = entity.connectionDate
+        val connectionDate     = entity.connectionDate
         val connectionDuration = getDurationAsString(connectionDate.toLocalDateTime)
 
         println(s"-$name : ")
@@ -60,13 +62,13 @@ class NetworkCommand(networks: => Iterable[Network]) extends CommandExecutor {
     }
 
     private def getDurationAsString(from: LocalDateTime): String = {
-        var millis = Duration.between(from, LocalDateTime.now()).toMillis
+        var millis  = Duration.between(from, LocalDateTime.now()).toMillis
         var seconds = millis / 1000
         var minutes = seconds / 60
-        var hours = minutes / 60
-        var days = hours / 24
-        var months = days / 31
-        val years = (months / 12)
+        var hours   = minutes / 60
+        var days    = hours / 24
+        var months  = days / 31
+        val years   = (months / 12)
 
         millis %= 1000
         seconds %= 60
@@ -77,6 +79,5 @@ class NetworkCommand(networks: => Iterable[Network]) extends CommandExecutor {
 
         s"$years y, $months m, $days d, $hours h, $minutes m, $seconds s and $millis ms"
     }
-
 
 }
