56eedcfbbad52d31007496b3b555b97f8cb2ea5c
Maxime <maximebatista18@gmail.com>
2021-04-23 15:21:29 +0200

    Redesigning resource management

--- DebugExtension/src/main/scala/fr/linkit/plugin/debug/DebugExtension.scala
+++ DebugExtension/src/main/scala/fr/linkit/plugin/debug/DebugExtension.scala
@@ -13,15 +13,14 @@
 package fr.linkit.plugin.debug
 
 import fr.linkit.api.local.plugin.LinkitPlugin
-import fr.linkit.api.local.resource.AutomaticBehaviorOption
-import fr.linkit.api.local.resource.representation.{ResourceFile, ResourceFolder}
+import fr.linkit.api.local.resource.ResourceFolder
+import fr.linkit.api.local.resource.external.{ResourceFile, ResourceFolder}
+import fr.linkit.api.local.resource.representation.ResourceFolder
 import fr.linkit.api.local.system.AppLogger
 import fr.linkit.core.local.concurrency.pool.{BusyWorkerPool, DedicatedWorkerController}
 import fr.linkit.plugin.controller.ControllerExtension
 import fr.linkit.plugin.controller.cli.CommandManager
 import fr.linkit.plugin.debug.commands.{NetworkCommand, PuppetCommand}
-
-import scala.util.Try
 
 class DebugExtension extends LinkitPlugin {
 
@@ -46,11 +45,11 @@ class DebugExtension extends LinkitPlugin {
         val resources            = getContext.getAppResources
 
         println(s"resources = ${resources}")
-        val file = Try(resources.openResourceFile("Test.exe", true))
-                .getOrElse(resources.get[ResourceFile]("Test.exe"))
+        val file = resources.find[ResourceFile]("Test.exe")
+                .getOrElse(resources.openResourceFile("Test.exe", true))
 
-        val folder = Try(resources.openResourceFolder("MyFolder", false, AutomaticBehaviorOption.values(): _*))
-                .getOrElse(resources.get[ResourceFolder]("MyFolder"))
+        val folder = resources.find[ResourceFolder]("MyFolder")
+                .getOrElse(resources.openResourceFolder("MyFolder", false))
 
         println(s"file = ${file}")
         println(s"folder = ${folder}")
