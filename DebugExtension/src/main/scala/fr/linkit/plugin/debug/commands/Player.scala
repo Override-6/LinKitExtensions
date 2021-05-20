c5f93ad0dcc94cf452b6b15e95760ecb3f7b7cf1
Maxime <maximebatista18@gmail.com>
2021-05-20 23:16:22 +0200

    Finding ways to fix bugs due to the design of the RMI system

--- DebugExtension/src/main/scala/fr/linkit/plugin/debug/commands/Player.scala
+++ DebugExtension/src/main/scala/fr/linkit/plugin/debug/commands/Player.scala
@@ -12,15 +12,16 @@
 
 package fr.linkit.plugin.debug.commands
 
-import fr.linkit.api.connection.cache.repo.annotations.MethodControl
+import fr.linkit.api.connection.cache.repo.annotations.InvocationKind.ONLY_LOCAL
+import fr.linkit.api.connection.cache.repo.annotations.{MethodControl => MC}
 
 import scala.annotation.meta.getter
 
-case class Player(@MethodControl(localOnly = true)@getter id: Int,
-                  @MethodControl(localOnly = true)@getter owner: String,
-                  @MethodControl(localOnly = true)@getter var name: String,
-                  @MethodControl(localOnly = true)@getter var x: Long,
-                  @MethodControl(localOnly = true)@getter var y: Long) extends Serializable {
+case class Player(@MC(ONLY_LOCAL) @getter id: Int,
+                  @MC(ONLY_LOCAL) @getter owner: String,
+                  @MC(ONLY_LOCAL) @getter var name: String,
+                  @MC(ONLY_LOCAL) @getter var x: Long,
+                  @MC(ONLY_LOCAL) @getter var y: Long) extends Serializable {
 
     def this(other: Player) = {
         this(other.id, other.owner, other.name, other.x, other.y)
@@ -28,6 +29,6 @@ case class Player(@MethodControl(localOnly = true)@getter id: Int,
 
     def getName: String = name
 
-    @MethodControl(localOnly = true)
+    @MC()
     override def toString: String = s"Player($id, $owner, $name, $x, $y)"
 }
